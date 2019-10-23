b0fcc4f068777368a731caefc645964a034e65c7
José Luis Recio <reciojoselu@gmail.com>
2019-10-23 16:55:19 +0200

    Added Angular exercise

---
+++ Frameworks/Angular/src/app/members/members-table/members-table.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+import { MemberEntity } from '../models/member.model';
+import { MembersApiService } from '../members-api.service';
+
+@Component({
+  selector: 'app-members-table',
+  templateUrl: './members-table.component.html',
+  styles: []
+})
+export class MembersTableComponent {
+  members: MemberEntity[];
+  organization = 'lemoncode';
+
+  constructor(private membersApi: MembersApiService) { }
+
+  loadMembers() {
+    this.membersApi.getAllMembers(this.organization)
+      .subscribe((ms) => this.members = ms);
+  }
+
+  setOrganization(newOrganization: string): void{
+    this.organization = newOrganization;
+  }
+
+}
